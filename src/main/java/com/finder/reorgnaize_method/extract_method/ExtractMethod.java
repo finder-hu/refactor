700b29799c95979c8eb2fb7a68ec15e304e131a5
longhuai.hlh <longhuai.hlh@alipay.com>
2016-11-16 10:04:00 +0800

    extract mathod: no local variable ,before refactor

---
+++ src/main/java/com/finder/reorgnaize_method/extract_method/ExtractMethod.java
@@ -0,0 +1,57 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved/
+ */
+package com.finder.reorgnaize_method.extract_method;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+/**
+ *
+ * @author Finderhu
+ * @version $Id: ExtractMethod.java, V 0.1 2016-11-16 9:51 Finderhu Exp $
+ */
+public class ExtractMethod {
+    private Vector<Order> orders = new Vector<Order>();
+    private String name;
+
+    public void printOwing(){
+        Enumeration e = this.orders.elements();
+        Double outstanding = 0.0;
+
+        //print banner
+        System.out.println("*************************");
+        System.out.println("***** Customer Owes *****");
+        System.out.println("*************************");
+
+        //caculate outstanding
+        while(e.hasMoreElements()){
+            Order each = (Order)e.nextElement();
+            outstanding += each.getAmount();
+        }
+
+        //print details
+        System.out.println("name:" + this.name);
+        System.out.println("amount:" + outstanding);
+    }
+
+    /**
+     * Getter method for property <tt>name</tt>.
+     *
+     * @return property value of name
+     */
+
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Setter method for property <tt>name</tt>.
+     *
+     * @param name value to be assigned to property name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/finder/reorgnaize_method/extract_method/Order.java
@@ -0,0 +1,40 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved/
+ */
+package com.finder.reorgnaize_method.extract_method;
+
+/**
+ *
+ * @author Finderhu
+ * @version $Id: Order.java, V 0.1 2016-11-16 9:54 Finderhu Exp $
+ */
+public class Order {
+    private int amount;
+
+    public Order(int amount) {
+        this.amount = amount;
+    }
+
+    public Order() {
+    }
+
+    /**
+     * Getter method for property <tt>amount</tt>.
+     *
+     * @return property value of amount
+     */
+
+    public int getAmount() {
+        return amount;
+    }
+
+    /**
+     * Setter method for property <tt>amount</tt>.
+     *
+     * @param amount value to be assigned to property amount
+     */
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}

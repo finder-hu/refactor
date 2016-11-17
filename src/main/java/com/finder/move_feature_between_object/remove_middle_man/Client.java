d6d8134f4b79d8ef10e8abfb192a19e75024ea1e
longhuai.hlh <longhuai.hlh@alipay.com>
2016-11-17 11:51:16 +0800

    Remove Middle Man: before refactor

---
+++ src/main/java/com/finder/move_feature_between_object/remove_middle_man/Client.java
@@ -0,0 +1,21 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved/
+ */
+package com.finder.move_feature_between_object.remove_middle_man;
+
+import com.finder.move_feature_between_object.hide_delegate.person.Person;
+
+/**
+ *
+ * @author Finderhu
+ * @version $Id: Client.java, V 0.1 2016-11-17 11:40 Finderhu Exp $
+ */
+public class Client {
+    private Person person;
+
+    public void someWork(){
+        Person manager = person.getManager();
+    }
+}
+
---
+++ src/main/java/com/finder/move_feature_between_object/remove_middle_man/person/Person.java
@@ -0,0 +1,28 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved/
+ */
+package com.finder.move_feature_between_object.remove_middle_man.person;
+
+/**
+ *
+ * @author Finderhu
+ * @version $Id: Person.java, V 0.1 2016-11-17 11:40 Finderhu Exp $
+ */
+public class Person {
+    private Department department;
+
+
+    public Person getManager(){
+        return department.getManager();
+    }
+
+    /**
+     * Setter method for property <tt>department</tt>.
+     *
+     * @param department value to be assigned to property department
+     */
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+}

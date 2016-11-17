47aeb99c4540995710b7398547e9bad9f3824969
longhuai.hlh <longhuai.hlh@alipay.com>
2016-11-17 13:07:37 +0800

    Introduce Local Extension: before refactor

---
+++ src/main/java/com/finder/move_feature_between_object/intoduce_local_extension/IntroduceForeignMethod.java
@@ -0,0 +1,24 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2016 All Rights Reserved/
+ */
+package com.finder.move_feature_between_object.intoduce_local_extension;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Finderhu
+ * @version $Id: IntroduceForeignMethod.java, V 0.1 2016-11-17 12:02 Finderhu Exp $
+ */
+public class IntroduceForeignMethod {
+    public void refactor(){
+        Date previousEnd = new Date();
+        Date newStart = nextDay(previousEnd);
+    }
+
+    private Date nextDay(Date previousEnd){
+        return new Date(previousEnd.getYear(),
+                previousEnd.getMonth(),previousEnd.getDate()+1);
+    }
+}

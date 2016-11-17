c4be87747bc9a28320752f29bef8b60e2e7e5f09
longhuai.hlh <longhuai.hlh@alipay.com>
2016-11-17 11:31:40 +0800

    Inline class: after refactor

--- src/main/java/com/finder/move_feature_between_object/inline_class/Person.java
+++ src/main/java/com/finder/move_feature_between_object/inline_class/Person.java
@@ -4,8 +4,6 @@
  */
 package com.finder.move_feature_between_object.inline_class;
 
-import com.finder.move_feature_between_object.extract_class.TelephoneNumber;
-
 /**
  *
  * @author Finderhu
@@ -13,7 +11,52 @@ import com.finder.move_feature_between_object.extract_class.TelephoneNumber;
  */
 public class Person {
     private String name;
-    private TelephoneNumber telephoneNumber = new TelephoneNumber();
+    private String areaCode;
+    private String officeNumber;
+
+    public String getTelephoneNumber(){
+        return ("(" + getAreaCode() + ")" + getOfficeNumber());
+    }
+
+
+    /**
+        * Getter method for property <tt>officeNumber</tt>.
+        *
+        * @return property value of officeNumber
+    */
+
+    public String getOfficeNumber() {
+        return officeNumber;
+    }
+
+    /**
+     * Setter method for property <tt>officeNumber</tt>.
+     *
+     * @param officeNumber value to be assigned to property officeNumber
+     */
+    public void setOfficeNumber(String officeNumber) {
+        this.officeNumber = officeNumber;
+    }
+
+    /**
+     * Getter method for property <tt>areaCode</tt>.
+     *
+     * @return property value of areaCode
+     */
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    /**
+     * Setter method for property <tt>areaCode</tt>.
+     *
+     * @param areaCode value to be assigned to property areaCode
+     */
+    public void setAreaCode(String areaCode) {
+        this.areaCode = areaCode;
+    }
+
 
     /**
      * Getter method for property <tt>name</tt>.

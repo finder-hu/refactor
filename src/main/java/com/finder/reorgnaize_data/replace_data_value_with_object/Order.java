9ed2e32adf9f962a50f3faa8c7c43f1491beed69
longhuai.hlh <longhuai.hlh@alipay.com>
2016-11-24 10:16:21 +0800

    Replace value with Object: before refactor

---
+++ src/main/java/com/finder/reorgnaize_data/replace_data_value_with_object/Order.java
@@ -0,0 +1,21 @@
+package com.finder.reorgnaize_data.replace_data_value_with_object;
+
+/**
+ * Created by longhuai.hlh on 2016/11/24.
+ */
+public class Order {
+    private String customer;
+
+    public Order(String customer) {
+
+        this.customer = customer;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+}
---
+++ src/main/java/com/finder/reorgnaize_data/replace_data_value_with_object/OrderCaller.java
@@ -0,0 +1,21 @@
+package com.finder.reorgnaize_data.replace_data_value_with_object;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * Created by longhuai.hlh on 2016/11/24.
+ */
+public class OrderCaller {
+    private static int numberOfOrdersFor(Collection orders, String customer){
+        int result = 0;
+        Iterator iter = orders.iterator();
+        while(iter.hasNext()){
+            Order order = (Order)iter.next();
+            if(order.getCustomer().equals(customer)){
+                result++;
+            }
+        }
+        return result;
+    }
+}
